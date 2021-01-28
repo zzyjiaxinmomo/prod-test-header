5e39ca968d530d2f0dbcaad8d8bb88166108c8d7
zhengzhiyuan <zzyjiaxinmomo@163.com>
2021-01-28 11:20:50 +0800

    Add Module Federation Config

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Header from "./Header";
 
 import "./index.css";
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+const App = () => (
+  <div>
+    <Header />
+    Hi there, I'm React from Webpack 5.
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById("app"));
